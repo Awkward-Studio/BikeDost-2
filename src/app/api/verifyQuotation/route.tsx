b95813a74ecbd64db923e6afc285734587b26d91
omi265 <omkarmishra22@gmail.com>
2025-04-08 20:42:11 +0530

    whatsapp http test

---
+++ src/app/api/verifyQuotation/route.tsx
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  //   console.log("BODY", request.body);
+  try {
+    const recievedData = await request.json();
+
+    //   await updateJobCardField()
+    console.log("THIS IS THE RECIEVED DATA _ ", recievedData);
+    return NextResponse.json({
+      message: "Recieved Message",
+      status: true,
+      recievedData,
+    });
+  } catch (error) {
+    console.log("Failed");
+    console.log(error);
+
+    return NextResponse.json({
+      message: "Failed",
+      status: false,
+    });
+  }
+}
